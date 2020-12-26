6e0a38e26fb12c4fc8498ff541741d9e072075db
Giancarlo Rocha <giancarloiff@gmail.com>
2020-12-26 12:10:04 -0300

    todo on notification processing

--- app/usecase/notification/process.rb
+++ app/usecase/notification/process.rb
@@ -5,6 +5,8 @@ class Notification::Process < Micro::Case
   attribute :notification_data
 
   def call!
+    # TODO: Since signature is handled every day, this should do nothing.
+    #       Eventualy it should update subscription status if necessary.
     Success result: { data: notification_data }
   end
 end
