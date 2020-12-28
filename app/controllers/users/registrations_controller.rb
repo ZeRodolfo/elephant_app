5ab5df98bfa2214666229b31908ee8327c0ab907
EnzoRobaina <enzo.robaina@gmail.com>
2020-12-28 00:50:05 -0300

    Adiciona validação e submit no form de registro

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,7 +10,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:category, :email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :phone, :document_number)
   end
 
   def account_update_params
