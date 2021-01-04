4af799655be34f3dee2f9c210e6292f7324259f5
EnzoRobaina <enzo.robaina@gmail.com>
2021-01-04 14:27:15 -0300

    Modifica controller das páginas static

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,8 @@
 class StaticController < ApplicationController
     skip_before_action :authenticate_user!
-    def show
-        render params[:page]
+    def landing
+    end
+
+    def termos
     end
 end
