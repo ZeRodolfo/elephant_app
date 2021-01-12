bb65e7b6019a78d59d8826e59faf0162dfa6b077
EnzoRobaina <enzo.robaina@gmail.com>
2021-01-11 22:42:11 -0300

    Remove turbolinks de calendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function () {
+$(function () {
     const selected = {
         start: {},
         end: {}
@@ -118,80 +118,7 @@ $(document).on('turbolinks:load', function () {
         selectionInfo.date = dateString
         selectionInfo.currentColor = color
     }
-
-    $('#submit').on('click', (event) => {
-        // clearErrors()
-        // $('#visits').val('')
-
-        // const startDate = new Date(toYyyyMmDd($start.val()) + 'T00:00:00')
-        // const endDate = $end.val() ? new Date(toYyyyMmDd($end.val()) + 'T00:00:00') : null
-        // const hour = $hour.val()
-
-        // if (endDate && isInvalid(startDate, endDate)){ 
-        //     const errorMsg = "A data inicial precisa ser menor que a final."
-
-        //     $start.addClass('is-invalid')
-        //     $end.addClass('is-invalid')
-
-        //     $start.parent().closest('div.invalid-feedback').html(errorMsg)
-        //     $end.parent().closest('div.invalid-feedback').html(errorMsg)
-        //     event.preventDefault() 
-        //     return
-        // }
-
-        // const repeat = parseInt($('#repetition-select :selected').val())
-        // let visits = []
-        // switch (repeat) {
-        //     default: case 0:
-        //         visits = [ buildVisit(startDate, hour) ]
-        //         break;
-        //     case SEMANAL:
-        //         visits = repeatWeekly(startDate, endDate, hour)
-        //     break;
-        //     case QUINZ:
-        //         visits = repeatQuinzenally(startDate, endDate, hour)
-        //     break;
-        //     case MENSAL:
-        //         visits = repeatMonthly(startDate, endDate, hour)
-        //     break;
-        // }
-
-        // console.log(visits)
-
-        // $('#visits').val(JSON.stringify(visits))
-
-        // const startDate = $start.val() ? new Date(toYyyyMmDd($start.val()) + 'T00:00:00').toISO() : ""
-        // const endDate = $end.val() ? new Date(toYyyyMmDd($end.val()) + 'T00:00:00').toISO()  : ""
-
-        // $start.val(startDate)
-        // $end.val(endDate)
-    })
-
-    function clearErrors() {
-        const form = $('form')[0]
-      
-        const fields = Array.from(form.elements)
-      
-        for (const field of fields) {
-          field.classList.remove('is-invalid')
-        }
-    }
-
-    // function isInvalid(start, end){ 
-    //     const invalidity = !isDateLessOrEqual(start, end)
-    //     if (invalidity){
-    //         $start.addClass('is-invalid')
-    //         $end.addClass('is-invalid')
-    //     }
-    //     return invalidity
-    // }
-
-    function isInvalid(start, end){ 
-        return !isDateLessOrEqual(start, end)
-    }
 })
-
-
 
 function toLocale(mmDdYyyy) {
     const [year, month, day] = mmDdYyyy.split('-') // 2021-01-23
@@ -213,81 +140,6 @@ function toYyyyMmDd(ddMmYyyy) {
     return `${year}-${month}-${day}`
 }
 
-function canAddDays(startDate, endDate, days) {
-    let _s = new Date(startDate)
-    _s.setDate(_s.getDate() + days)
-    return _s.getTime() <= endDate.getTime()
-}
-
-function repeatWeekly(startDate, endDate, hour) {
-    return _repeatByDays(startDate, endDate, 7, hour)
-}
-
-function _repeatByDays(startDate, endDate, interval, hour){
-    const dates = [buildVisit(startDate, hour)]
-    while (canAddDays(startDate, endDate, interval)) {
-        startDate.setDate(startDate.getDate() + interval)
-        dates.push(buildVisit(new Date(startDate), hour))
-    }
-    return dates
-}
-
-function repeatQuinzenally(startDate, endDate, hour) {
-    return _repeatByDays(startDate, endDate, 15, hour)
-}
-
-function canAdd30(startDate, endDate) {
-    let _s = new Date(startDate)
-    add1Month(_s)
-    return isDateLessOrEqual(_s, endDate)
-}
-
-function isDateLessOrEqual(date1, date2) {
-    let _date1 = new Date(
-        date1.getFullYear(),
-        date1.getMonth(),
-        date1.getDay(), 0, 0, 0, 0
-    )
-
-    let _date2 = new Date(
-        date2.getFullYear(),
-        date2.getMonth(),
-        date2.getDay(), 0, 0, 0, 0
-    )
-
-    return _date1 <= _date2
-}
-
-function repeatMonthly(startDate, endDate, hour) {
-    const dates = [buildVisit(new Date(startDate), hour)]
-    while (canAdd30(startDate, endDate)) {
-        add1Month(startDate)
-        dates.push(
-            buildVisit(new Date(startDate), hour),
-        )
-    }
-    return dates
-}
-
-function isLeapYear(year) {
-    return (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0))
-}
-
-function getDaysInMonth(year, month) {
-    return [31, (isLeapYear(year) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month]
-}
-
-function add1Month(date) {
-    date.setMonth(date.getMonth() + 1)
-}
-
 Date.prototype.toISO = function(){
     return JSON.stringify(this).split('T')[0].replace('"', '')
-}
-
-function buildVisit(date, hour){
-    return {
-        date: date instanceof Date ? date.toISO() : date,
-        hour
-    }
 }
