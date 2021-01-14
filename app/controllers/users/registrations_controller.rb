a992b4b971fb72f0f2a4a1a864af8bdcb980266c
Giancarlo Rocha <giancarloiff@gmail.com>
2021-01-14 17:35:24 -0300

    altera url após user update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,6 +8,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     resource.update_without_password(params)
   end
 
+  def after_update_path_for(resource)
+    edit_user_registration_path
+  end
+
   private
 
   def sign_up_params
