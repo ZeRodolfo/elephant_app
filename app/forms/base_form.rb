48837c93c28e98ffc2f3ed7789a9a17b1d12c71b
Giancarlo Rocha <giancarloiff@gmail.com>
2021-01-21 19:26:44 -0300

    adiciona metodos no base form

--- app/forms/base_form.rb
+++ app/forms/base_form.rb
@@ -1,30 +1,54 @@
 class BaseForm
-    attr_reader :fields
-    def initialize
-        @fields = []
-    end
+  attr_reader :fields
 
-    def fill(data)
-        data.each do |d|
-            if d[0].present?
-                field = @fields.detect { |f| f[:name] == d[0] }
-                unless field.nil?
-                    field[:value] = (field[:type] == 'text' || field[:type] == 'textarea') ? d[1].chomp : d[1]
-                end
-            end
+  def initialize
+    @fields = []
+  end
+
+  def fill(data)
+    data.each do |d|
+      if d[0].present?
+        field = @fields.detect { |f| f[:name] == d[0] }
+        unless field.nil?
+          field[:value] = (field[:type] == "text" || field[:type] == "textarea") ? d[1].chomp : d[1]
         end
+      end
     end
+  end
 
-    def serialize
-        @fields.each_with_object({}) { |f, hash| hash[f[:name]] = f[:value] }.to_json
-    end
+  def serialize
+    @fields.each_with_object({}) { |f, hash| hash[f[:name]] = f[:value] }.to_json
+  end
 
-    def deserialize(data_json)
-        data = JSON.parse(data_json)
-        self.fill(data.to_a)
-    end
+  def deserialize(data_json)
+    data = JSON.parse(data_json)
+    self.fill(data.to_a)
+  end
 
-    def clear
-        @fields.each{ |f| f.except!(:value) }
+  def clear
+    @fields.each { |f| f.except!(:value) }
+  end
+
+  def each_field(*field_names, &block)
+    field_names.each do |field_name|
+      block.call self[field_name]
     end
+  end
+
+  def row_of(*field_names)
+    field_names.map do |field_name|
+      [self[field_name].label, self[field_name].value || '']
+    end
+  end
+
+  def method_missing(name, *args, &block)
+    field_name = args.first
+    field_name = name unless name == :[]
+
+    field = fields.find { |f| f[:name]&.to_sym == field_name }
+
+    return nil unless field
+
+    OpenStruct.new(field)
+  end
 end
