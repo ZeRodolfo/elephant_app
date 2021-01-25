70e7fcbb9b2431c7b19bb001a2131a45e5f64297
Giancarlo Rocha <giancarloiff@gmail.com>
2021-01-25 11:07:49 -0300

    fix exclusion of user preferences

--- app/models/user_preference.rb
+++ app/models/user_preference.rb
@@ -9,7 +9,7 @@
 class UserPreference < ApplicationRecord
   include ActiveStorage::Downloading
 
-  has_one :user
+  has_one :user, dependent: :nullify
   has_one_attached :papel_timbrado
 
   validates :papel_timbrado, content_type: %w[image/png image/jpg image/jpeg]
