f45dff2a332f5c8035c5dacf189e6ff9d59ca573
EnzoRobaina <enzo.robaina@gmail.com>
2021-03-10 15:32:23 -0300

    Closes #18 | Adiciona novas formas de vis. das finanças

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,65 +1,248 @@
-$(document).on('turbolinks:load', () => {
-  const graphs = [
-    Morris.Line({
-      element: 'week-graph',
-      data: $('#week-graph').data('graph'),
-      xkey: 'created_at',
-      ykeys: ['collected'],
-      labels: ['Quantia Recebida (R$)'],
-      lineColors: ['#45AA77']
+$(document).on("turbolinks:load", () => {
+  // const graphs = [
+  //   Morris.Line({
+  //     element: 'week-graph',
+  //     data: $('#week-graph').data('graph'),
+  //     xkey: 'created_at',
+  //     ykeys: ['collected'],
+  //     labels: ['Quantia Recebida (R$)'],
+  //     lineColors: ['#45AA77']
+  //   }),
+
+  //   Morris.Line({
+  //     element: 'month-graph',
+  //     data: $('#month-graph').data('graph'),
+  //     xkey: 'created_at',
+  //     ykeys: ['collected'],
+  //     labels: ['Quantia Recebida (R$)'],
+  //     lineColors: ['#45AA77']
+  //   }),
+
+  //   Morris.Line({
+  //     element: 'annual-graph',
+  //     data: $('#annual-graph').data('graph'),
+  //     xkey: 'created_at',
+  //     ykeys: ['collected'],
+  //     labels: ['Quantia Recebida (R$)'],
+  //     lineColors: ['#45AA77']
+  //   })
+  // ]
+
+
+  Chart.defaults.global.plugins = {
+    datalabels: {
+      color: "black",
+      font: {
+        size: 14,
+        weight: "bold",
+        family: '"Lucida Console", Monaco, monospace',
+      },
+      align: "end",
+      anchor: "end",
+    },
+  }
+  Chart.defaults.global.maintainAspectRatio = true
+  Chart.defaults.global.responsive = true
+  Chart.defaults.global.defaultColor = "#45AA77"
+  Chart.defaults.global.tooltips.enabled = false
+  Chart.defaults.global.title.display = true
+  Chart.defaults.global.legend.display = false
+  Chart.plugins.register(ChartDataLabels)
+
+  const weekEl = document.getElementById("week-graph");
+  const weekData = JSON.parse(
+    weekEl.dataset["graph"]
+  );
+
+  const monthEl = document.getElementById("month-graph");
+  const monthData = JSON.parse(
+    monthEl.dataset["graph"]
+  );
+
+  const yearEl = document.getElementById("annual-graph");
+  const yearData = JSON.parse(
+    yearEl.dataset["graph"]
+  );
+
+  const charts = [
+    new Chart(weekEl.getContext("2d"), {
+      type: "bar",
+      data: {
+        labels: weekData.map((d) => d.label),
+        datasets: [
+          {
+            backgroundColor: "#45AA77",
+            label: "Rendimentos",
+            data: weekData.map((d) => d.value),
+          },
+        ],
+      },
+      options: {
+        title: {
+          text: "Rendimento em intervalos semanais",
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                suggestedMin: 0,
+                suggestedMax: 1000,
+                stepSize: 100,
+                beginAtZero: true,
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "Valor em R$",
+              },
+            },
+          ],
+        },
+      },
     }),
 
-    Morris.Line({
-      element: 'month-graph',
-      data: $('#month-graph').data('graph'),
-      xkey: 'created_at',
-      ykeys: ['collected'],
-      labels: ['Quantia Recebida (R$)'],
-      lineColors: ['#45AA77']
+    new Chart(monthEl.getContext("2d"), {
+      type: "bar",
+      data: {
+        labels: monthData.map((d) => d.label),
+        datasets: [
+          {
+            backgroundColor: "#45AA77",
+            label: "Rendimentos",
+            data: monthData.map((d) => d.value),
+          },
+        ],
+      },
+      options: {
+        title: {
+          text: "Rendimento em intervalos mensais",
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                suggestedMin: 0,
+                suggestedMax: 1000,
+                stepSize: 100,
+                beginAtZero: true,
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "Valor em R$",
+              },
+            },
+          ],
+        },
+      },
     }),
 
-    Morris.Line({
-      element: 'annual-graph',
-      data: $('#annual-graph').data('graph'),
-      xkey: 'created_at',
-      ykeys: ['collected'],
-      labels: ['Quantia Recebida (R$)'],
-      lineColors: ['#45AA77']
-    })
-  ]
+    new Chart(yearEl.getContext("2d"), {
+      type: "bar",
+      data: {
+        labels: yearData.map((d) => d.label),
+        datasets: [
+          {
+            backgroundColor: "#45AA77",
+            label: "Rendimentos",
+            data: yearData.map((d) => d.value),
+          },
+        ],
+      },
+      options: {
+        title: {
+          text: "Rendimento por ano",
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                suggestedMin: 0,
+                suggestedMax: 10000,
+                stepSize: 1000,
+                beginAtZero: true,
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "Valor em R$",
+              },
+            },
+          ],
+        },
+      },
+    }),
+  ];
 
-  function getValueAndShow() {
-    const value = parseInt(this.value)
+  const dailyEl = $('#daily-data')
+  const dailyData = dailyEl.data('values')
+  const dailyPicker = dailyEl.datepicker({
+    startDate: dailyEl.data('start'),
+    endDate: dailyEl.data('end'),
+    defaultViewDate: 'today',
+    maxViewMode: 1,
+    language: "pt-BR",
+    todayHighlight: true
+  })
 
-    show(value, () => {
-      graphs[value].redraw()
-    })
+  const today = new Date()
+  today.setHours(0,0,0,0)
+
+  dailyPicker.on('changeDate', function(e){ showValueForDay(e.date) })
+
+  dailyPicker.datepicker('setDate', today)
+
+  function showValueForDay(day){
+    if (!(day instanceof Date)){ return }
+
+    $('#selected-day').text(locale(day))
+    $('#value').text(dailyData[day.getTime() / 1000] || 0)
   }
 
-  const graphSelect = $('#graph-interval-select')
+  function locale(date){
+    if (!(date instanceof Date)){ return 'Inválido.'}
+
+    return date.toLocaleDateString('pt-BR')
+  }
+
+  function getValueAndShow() {
+    const value = parseInt(this.value);
+
+    show(value);
+  }
+
+  const graphSelect = $("#graph-interval-select");
 
   graphSelect.on('change', getValueAndShow)
 
-  graphSelect.trigger('change')
+  graphSelect.trigger("change");
 
   function hideAll() {
-    $('div[data-select]').hide()
+    $("div[data-select]").addClass('visuallyhidden');
   }
 
-  function show(identifier, callback) {
-    hideAll()
-    $(`div[data-select="${identifier}`).show(callback)
+  function show(identifier) {
+    hideAll();
+    $(`div[data-select="${identifier}`).removeClass('visuallyhidden');
   }
 
   function getCurrentVisibleIndex() {
-    return parseInt(getCurrentVisible()[0].dataset['select'])
+    return parseInt(getCurrentVisible()[0].dataset["select"]);
   }
 
   function getCurrentVisible() {
-    return $('div[data-select]:visible')
+    return $("div[data-select]:not(.visuallyhidden)");
   }
 
-  window.addEventListener('optimizedResize', function () {
-    graphs[getCurrentVisibleIndex()].redraw()
-  })
-})
+  window.addEventListener("optimizedResize", function () {
+    if (window.innerWidth <= 768 && Chart.defaults.global.maintainAspectRatio){
+      Chart.defaults.global.maintainAspectRatio = false;
+    }
+    else {
+      if (!Chart.defaults.global.maintainAspectRatio)
+        Chart.defaults.global.maintainAspectRatio = true
+    }
+
+    for (const chart of charts){
+      chart.options.maintainAspectRatio = Chart.defaults.global.maintainAspectRatio;
+      chart.update()
+    }
+  });
+});
--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -1,29 +1,74 @@
 module GraphsHelper
 	def week_data
-	    (4.weeks.ago.to_date..Date.today).map do |date| {
-	      created_at: date,
-	      collected: parcels_value(date)
-	    }
-	    end
+		start_date = 1.month.ago.to_date
+		end_date = 1.week.ago.to_date
+		data = []
+
+		time_iterate(start_date, end_date, 1.week) do |date| 
+			e = date + 1.week
+			data.push(
+				{
+					label: [locale(date), ' até ', locale(e)],
+					value: range(date, e)
+				}
+			)
+		end
+		
+		data
+	end
+  	
+
+	def month_data
+		start_date = 6.months.ago.to_date
+		end_date = Date.today + 6.months
+		data = []
+		time_iterate(start_date, end_date, 1.month) do |date| 
+			data.push({
+				label: [locale_month(date.month), ' de ', date.year.to_s],
+				value: range(date, date + 1.month)
+			})
+		end
+		
+		data
   	end
 
-  	def month_data
-	    (4.months.ago.to_date..Date.today).map do |date| {
-	      created_at: date,
-	      collected: parcels_value(date)
-	    }
-	    end
-  	end
+	def annual_data
+		start_date = Date.today
+		end_date = Date.today + 4.years
+		data = []
+		time_iterate(start_date, end_date, 1.year) do |date| 
+			data.push({
+				label: date.year.to_s,
+				value: range(date, date + 1.year)
+			})
+		end
 
-  	def annual_data
-	    (4.years.ago.to_date..Date.today).map do |date| {
-	      created_at: date,
-	      collected: parcels_value(date)
-	    }
-	    end
-  	end
+		data
+	end
+
+	def daily_data
+		(1.month.ago.to_date..Date.today + 1.month).map{ |date| [date.to_time.to_i, parcels_value(date)] }.to_h
+	end
+	  
+	def range(start_date, end_date)
+		OfficeVisit.joins(:patient).joins(:parcels).where("(date(office_visits.created_at) >= ? AND date(office_visits.created_at) < ?) AND patients.user_id = ?", start_date, end_date, current_user.id).sum("parcels.value")
+	end
 
   	def parcels_value(date)
   		OfficeVisit.joins(:patient).joins(:parcels).where("date(office_visits.created_at) = ? AND patients.user_id = ?", date, current_user.id).sum("parcels.value")
-  	end
+	  end
+	  
+	def time_iterate(start_time, end_time, step, &block)
+		begin
+			yield(start_time)
+		end while (start_time += step) <= end_time
+	end
+
+	def locale(date)
+		date.strftime('%d/%m/%Y')
+	end
+
+	def locale_month(index)
+		%w[nil Janeiro Fevereiro Março Abril Maio Junho Julho Agosto Setembro Outubro Novembro Dezembro][index]
+	end
 end
