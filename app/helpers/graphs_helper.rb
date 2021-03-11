87b3833716d7efe024a9cf9a01433515efe7db80
Giancarlo Rocha <giancarloiff@gmail.com>
2021-03-11 20:34:51 -0300

    fix graphs

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -4,7 +4,7 @@ module GraphsHelper
 		end_date = 1.week.ago.to_date
 		data = []
 
-		time_iterate(start_date, end_date, 1.week) do |date| 
+		time_iterate(start_date, end_date, 1.week) do |date|
 			e = date + 1.week
 			data.push(
 				{
@@ -13,22 +13,21 @@ module GraphsHelper
 				}
 			)
 		end
-		
+
 		data
 	end
-  	
 
 	def month_data
 		start_date = 6.months.ago.to_date
 		end_date = Date.today + 6.months
 		data = []
-		time_iterate(start_date, end_date, 1.month) do |date| 
+		time_iterate(start_date, end_date, 1.month) do |date|
 			data.push({
 				label: [locale_month(date.month), ' de ', date.year.to_s],
 				value: range(date, date + 1.month)
 			})
 		end
-		
+
 		data
   	end
 
@@ -36,7 +35,7 @@ module GraphsHelper
 		start_date = Date.today
 		end_date = Date.today + 4.years
 		data = []
-		time_iterate(start_date, end_date, 1.year) do |date| 
+		time_iterate(start_date, end_date, 1.year) do |date|
 			data.push({
 				label: date.year.to_s,
 				value: range(date, date + 1.year)
@@ -49,15 +48,15 @@ module GraphsHelper
 	def daily_data
 		(1.month.ago.to_date..Date.today + 1.month).map{ |date| [date.to_time.to_i, parcels_value(date)] }.to_h
 	end
-	  
+
 	def range(start_date, end_date)
-		OfficeVisit.joins(:patient).joins(:parcels).where("(date(office_visits.created_at) >= ? AND date(office_visits.created_at) < ?) AND patients.user_id = ?", start_date, end_date, current_user.id).sum("parcels.value")
+		OfficeVisit.joins(:patient).joins(:parcels).where("(date(parcels.created_at) >= ? AND date(parcels.created_at) <= ?) AND patients.user_id = ?", start_date, end_date, current_user.id).sum("parcels.value")
 	end
 
-  	def parcels_value(date)
-  		OfficeVisit.joins(:patient).joins(:parcels).where("date(office_visits.created_at) = ? AND patients.user_id = ?", date, current_user.id).sum("parcels.value")
-	  end
-	  
+	def parcels_value(date)
+		OfficeVisit.joins(:patient).joins(:parcels).where("date(parcels.created_at) = ? AND patients.user_id = ?", date, current_user.id).sum("parcels.value")
+	end
+
 	def time_iterate(start_time, end_time, step, &block)
 		begin
 			yield(start_time)
