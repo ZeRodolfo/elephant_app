59d84e4db8ba2e16644774810cf023a4007e888e
Rodolfo Pessanha <rodolfo.silva@solides.com.br>
2022-01-26 10:12:29 -0300

    fix: added setup for start local with docker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,18 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.action_mailer.default_url_options = { host: 'elephantapp.com.br' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    #domain: "gmail.com",
+    port: "587",
+    user_name: "recuperacaodesenhaelephant@gmail.com",
+    password: "elephantsc2020",
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
